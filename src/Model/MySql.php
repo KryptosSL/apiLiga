b4a7bef7f822a0f673834d8cb195076abdab73da
MuriloSantosdeLima <66491812+MuriloSantosdeLima@users.noreply.github.com>
2022-09-15 23:05:39 -0300

    update mysql

--- src/Model/MySql.php
+++ src/Model/MySql.php
@@ -2,17 +2,14 @@
 
 class MySql{
 
-	private $host = "127.0.0.1";
-	private $name = "apiLiga";
-	private $user = "root";
-	private $pass = "root";
 	private $con;
 	public $stmt;
 	private $result;
-
-		public static $instance;
-		
-        public static function getInstance() {
+	public static $instance;
+	
+	
+	
+		public static function getInstance() {
 	        if (!isset(self::$instance)) {
 	            self::$instance = new PDO("mysql:host=127.0.0.1:3306;dbname=apiLiga", "root", "root",
 	 array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
